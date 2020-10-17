17986bac9eb6d3eee53cce72f86962638ad15aee
Javier salazar <jsalazar@lumston.com>
2020-10-17 13:50:40 -0500

    finish buble method

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,18 @@
 package main
 
-func Burbuja(s []int64)  {
-
+func Burbuja(s []int64) {
+	var temp int64
+	for j := 0; j < len(s); j++ {
+		for i := 0; i < len(s)-1; i++ {
+			if i < len(s) && s[i] > s[i+1] {
+				temp = s[i]
+				s[i] = s[i+1]
+				s[i+1] = temp
+			}
+		}
+	}
 }
 
-func main()  {
-	
+func main() {
+
 }
